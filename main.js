f58d9be9431c2ae918c09cd2a29099ddac4936a1
mohsin <imtiaz.ucb@gmail.com>
2021-06-03 16:04:21 +0600

    v0.0.1

---
+++ main.js
@@ -0,0 +1,14 @@
+var bars = document.getElementsByClassName("st0");
+// for (var i = 0; i < bars.length; i++) {
+//   bars[i].addEventListener("mouseover", mouseOverEffect);
+//   bars[i].addEventListener("mouseout", mouseOutEffect);
+// }
+
+// function mouseOverEffect() {
+//   this.classList.add("bar-highlight");
+// }
+// function mouseOutEffect() {
+//   this.classList.remove("bar-highlight");
+// }
+
+console.log("bars", bars);
